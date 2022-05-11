0513e0bb3b3c5e2de810d421584faadef162105a
AKIMANA-Olivier <105389793+AKIMANA-Olivier@users.noreply.github.com>
2022-05-11 21:57:59 +0200

    Add files via upload

---
+++ php_crud_files/header.php
@@ -0,0 +1,37 @@
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="with=device-with, initial-scale"
+=1,shrink-to-fit=no">
+<link rel="stylesheet" 
+href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" 
+integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" 
+crossorigin="anonymous">
+<link rel="stylesheet"
+ href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" 
+ integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" 
+ crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" 
+integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" 
+crossorigin="anonymous"></script>
+</head>
+<body class="bg-info">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse"
+  data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"   
+  aria-expanded="false" aria-label="toggle navigation">
+  <span class ="navbar-togglet-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+        <li class ="nav-item active">
+        <a class="btn btn-info" href="/pdo/assign">Home <span class="sr-only">(current)</span></a></li>
+        <li class ="nav-item active">
+        <a class="btn btn-info" href="/pdo/assign/create.php">Add a person </a></li>
+        <li class ="nav-item active">
+        <a class="btn btn-info" href="/pdo/assign/upload-csv.php">Upload CSV File </a></li>
+</ul>
+</div>
+</nav>
